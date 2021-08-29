c476ae92823f65a7dc18cce53a9209f145bb625b
penumbra23 <glbranimir@gmail.com>
2021-08-29 13:21:38 +0200

    Refactor exec_hook

--- src/core/hooks.rs
+++ src/core/hooks.rs
@@ -1,27 +1,34 @@
 use std::{io::Write, os::unix::prelude::CommandExt, process::Command};
 
-use crate::{core::state::State, oci::spec::Hook};
+use crate::core::{
+    common::{Error, ErrorType, Result},
+    state::State,
+};
 
-pub fn exec_hook(hook: &Hook, state: &State) {
-    let hook_cmd = hook.path.clone();
+use crate::oci::spec::Hook;
 
-    let mut hook_command = Command::new(hook_cmd);
+pub fn exec_hook(hook: &Hook, state: &State) -> Result<i32> {
+    let mut hook_command = Command::new(&hook.path);
 
     let arg0 = hook.args.as_ref().unwrap()[0].clone();
     let args = hook.args.as_ref().unwrap()[1..].to_vec();
     hook_command.arg0(&arg0).args(&args);
 
-    let hook_process: std::process::Child = hook_command
+    let mut hook_process: std::process::Child = hook_command
         .stdin(std::process::Stdio::piped())
         .spawn()
-        .unwrap();
+        .map_err(|err| Error { msg: err.to_string(), err_type: ErrorType::Runtime })?;
 
     // Provide container state to the hook
     // NOTE: the "pid" is the only important field for the libnetwork hook
     if let Some(mut stdin) = hook_process.stdin.as_ref() {
         let state_json = serde_json::to_string(state).unwrap();
-        stdin.write(state_json.as_bytes()).unwrap();
+        stdin.write(state_json.as_bytes()).map_err(|_| Error { msg: String::from("error writing to hook stdin"), err_type: ErrorType::Runtime })?;
     }
 
-    let output = hook_process.wait_with_output().unwrap();
+    let status = hook_process.wait().map_err(|_| Error { msg: String::from("error hook execution"), err_type: ErrorType::Runtime })?;
+    match status.code() {
+        Some(c) => Ok(c),
+        None => Err(Error { msg: String::from("hook interrupted by signal"), err_type: ErrorType::Runtime }),
+    }
 }
